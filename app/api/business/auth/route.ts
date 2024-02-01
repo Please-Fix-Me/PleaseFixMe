9fa2df35bc9454a65c5f1224803504bca8972010
Nicholas Ceccarelli <nceccarelli2@moog.com>
2024-02-01 12:02:23 -0500

    convert auth to use queryDb()

--- app/api/business/auth/route.ts
+++ app/api/business/auth/route.ts
@@ -22,41 +22,32 @@ export async function GET(request:NextRequest) {
 }
 
 export async function POST(request:NextRequest) {
-    const uri = process.env.DB_CONNECTION_STRING!
-    const db = process.env.DB_NAME!
-
     const body = await request.json()
 
-    const client = new MongoClient(uri);
-    var isAuth = false;
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        // Specify the database and collection
-        const database = client.db(db);
-        const collection = database.collection(BUSINESS_PASSWORD_COLLECTION_NAME);
-
-        // Find all entries in the collection
-        var data = await collection.find({name: body['name'], password: body['password']}).toArray();
-        if (data.length > 0) {
-            isAuth = true;
+    var response = await queryDb(
+        BUSINESS_PASSWORD_COLLECTION_NAME, 
+        {
+            name:  body['name'],
+            password: body['password']
         }
+    )
 
-    } finally {
-        // Close the connection to the MongoDB cluster
-        await client.close();
-    }
-
-    // console.log(businessNames)
-    if (isAuth) {
-        return NextResponse.json({})
+    // Parse response and return the correct result
+    if (response.success) {
+        if (response.result.length > 0) {
+            return NextResponse.json({})
+        } else {
+            return NextResponse.json({
+                message: "Incorrect password."
+            }, {
+                status: 401,
+            })
+        }
     } else {
         return NextResponse.json({
-            message: "Incorrect password."
+            message: response.result
         }, {
-            status: 401,
+            status: 400,
         })
     }
 }
