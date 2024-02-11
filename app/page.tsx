8963ef721e9106f5de4c63ac44f8b98d68bcf6ef
Nicholas Ceccarelli <nceccarelli2@moog.com>
2024-02-11 09:41:22 -0500

    Remove legacy bug display

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import BugDisplay from "./components/BugDisplay";
-import Link from 'next/link';
-import Button from "./components/Button";
 import LinkButton from "./components/LinkButton";
 
 export default function Home() {
@@ -10,13 +7,20 @@ export default function Home() {
         <h1 className="text-3xl pb-5">
           Welcome to PleaseFixMe!
         </h1>
-        <p>We are working on building the application and will have a version live soon.</p>
-        <BugDisplay />
 
+        <p className="pt-3">
+          If you are a user of products managed by PleaseFixMe and wish to report a defect, use the button below.
+        </p>
+        <LinkButton href={"TODO: "}>
+          TODO
+        </LinkButton>
+
+        <p className="pt-3">
+          If you are a business administrator or moderator, click the button below.
+        </p>
         <LinkButton href={"/business"}>
           Business Management
         </LinkButton>
-
       </div>
     </main>
   );
