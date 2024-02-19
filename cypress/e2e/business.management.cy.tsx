bdf643f6836b263004c52ddb131c4e7c5d33a4d8
sdavies39219 <stacey.davies@cengage.com>
2024-02-19 15:33:45 -0500

    Updates to flaky tests Started to map out what we'll need to do for defects

--- cypress/e2e/business.management.cy.tsx
+++ cypress/e2e/business.management.cy.tsx
@@ -1,8 +1,8 @@
 describe('Business Management Page ', () => {
     it('should navigate to business management', () => {
         //Start at website
-        cy.visit('https://pleasefixme.vercel.app/')
-        //cy.visit('http://localhost:3000/')
+        //  cy.visit('https://pleasefixme.vercel.app/')
+        cy.visit('http://localhost:3000/')
 
         // Find a link with an href attribute containing "Business Management" and click it
         cy.get('a[href*="/business"]').click()
@@ -60,8 +60,13 @@ describe('Business Management Page ', () => {
 
         //Enter product name and description
         cy.get('input[name="name"]').type('Product 1')
+        cy.get('textarea[name="description"]').click()
         cy.get('textarea[name="description"]').type('This is description for Product 1')
+        cy.get('input[type="file"]').attachFile('2DS_Attributes.png');
+        //   cy.contains('2DS_Attributes.png');
+        cy.wait(500)
         cy.get('input[type="submit"]').click()
+
 
         //Go back to products page and verify we see our product
         cy.get('a[href*="/business/offering?name=Automation Test"]').click()
@@ -69,14 +74,32 @@ describe('Business Management Page ', () => {
         cy.get('input[type="submit"]').click()
         cy.get('td').contains('Product 1')
 
+        //Go to defects page for Boston University
+        //Will need to update once all code gets merged into main
+        //cy.get('a[href*="/business"]').click('.btn')
+        // cy.get('button[class*="bg-white text-black py-1 px-3 rounded hover:bg-gray-200 cursor-pointer"]').click()
+        // cy.get('a[href*="/business/offering?name=Boston University"]').click()
+        // cy.get('input[name="password"]').type('0/k0t2qZK/Ak')
+        // cy.get('a[href*="/business/offering/defect?offeringName=Zoom&businessName=Boston University"]').click()
+        // cy.get('a[href*="/business/offering/defect/view?id=65d320d7b8e8a014ecba4669&businessName=Boston University&offeringName=Zoom"]').click()
+        // cy.get("select").select("Won't Fix")
+        // cy.get('textarea[name="statusChangeReason"]').click()
+        // cy.get('textarea[name="description"]').type('This is my reason')
+        // cy.get('input[type="submit"]').click()
+        // cy.get('td').contains('Wont Fix').should('exist')
+        // cy.get('a[href*="/business"]').click()
+
         //Edit our product, verify original text and changes are correct, and delete it
         cy.get('a[href*="/business/offering/edit?name=Product 1&businessName=Automation Test"]').click()
         cy.get('h1').contains('Edit Product 1')
         cy.get('textarea[name="description"]').contains('This is description for Product 1')
+        cy.get('textarea[name="description"]').click()
         cy.get('textarea[name="description"]').type('{selectall}{backspace}')
         cy.get('textarea[name="description"]').type('This is an update')
         cy.get('input[type="submit"]').click()
+        cy.wait(1000)
         cy.get('textarea[name="description"]').contains('This is an update')
+        cy.get('img[alt="Please upload an image to see it here"').should('exist');
 
         //Delete our product and verify it is no longer listed
         cy.get('button[class="bg-white text-black py-2 px-4 rounded hover:bg-gray-200 cursor-pointer"]').contains('Delete').click()
@@ -102,8 +125,12 @@ describe('Business Management Page ', () => {
         cy.get('textarea[name="description"]').contains('Automation Test Description')
 
         //Edit Business Description
+        cy.get('textarea[name="description"]').click()
+        cy.wait(500)
         cy.get('textarea[name="description"]').type('{selectall}{backspace}')
+        cy.wait(500)
         cy.get('textarea[name="description"]').type('Automation Test Description Update')
+        cy.wait(500)
         cy.get('input[type="submit"]').click()
         cy.wait(500)
         //Verify Update
@@ -113,8 +140,12 @@ describe('Business Management Page ', () => {
         cy.get('input[name="contactName"]').should('have.value', 'Automation First Name')
 
         //Edit Contact Person Name
+        cy.get('input[name="contactName"]').click()
+        cy.wait(500)
         cy.get('input[name="contactName"]').type('{selectall}{backspace}')
+        cy.wait(500)
         cy.get('input[name="contactName"]').type('Automation First Name Update')
+        cy.wait(500)
         cy.get('input[type="submit"]').click()
         //verify Update
         cy.wait(500)
@@ -122,10 +153,14 @@ describe('Business Management Page ', () => {
 
         //Verify the Contact email address we previously entered
         cy.get('input[name="contactEmail"]').should('have.value', 'Automation@test.com')
+        cy.wait(500)
 
         //Edit Contact email address
+        cy.get('input[name="contactEmail"]').click()
         cy.get('input[name="contactEmail"]').type('{selectall}{backspace}')
+        cy.wait(500)
         cy.get('input[name="contactEmail"]').type('AutomationUPDATED@test.com')
+        cy.wait(500)
         cy.get('input[type="submit"]').click()
         //verify Update
         cy.wait(500)
@@ -135,8 +170,11 @@ describe('Business Management Page ', () => {
         cy.get('input[name="contactPhone"]').should('have.value', '0000000000')
 
         //Edit Contact phone number
+        cy.get('input[name="contactPhone"]').click()
         cy.get('input[name="contactPhone"]').type('{selectall}{backspace}')
+        cy.wait(500)
         cy.get('input[name="contactPhone"]').type('1111111111')
+        cy.wait(500)
         cy.get('input[type="submit"]').click()
         //verify Update
         cy.wait(500)
@@ -149,7 +187,6 @@ describe('Business Management Page ', () => {
         cy.get('a[href*="/business"]').click()
 
         //Verify our business is no longer listed
-        cy.wait(500)
         cy.reload()
         cy.get('td').contains('Automation Test').should('not.exist')
     })
